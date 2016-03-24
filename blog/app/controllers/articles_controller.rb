996fa947b3cd4c9260b720ac3b2159a20f288e3d
DamingLeonHuang <DamingLeonHuang@gmail.com>
2016-03-24 13:54:51 -0400

    just compelete 5.11

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
-    
+
   end
 
   def show
@@ -23,6 +23,16 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
   def article_params
